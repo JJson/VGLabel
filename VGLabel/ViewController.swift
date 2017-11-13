ac17775f1aa4e9f7cbbb58762bb9aa01c0461fcc
Vein <gwrcode@gmail.com>
2017-11-13 11:57:22 +0800

    fix test

--- VGLabel/ViewController.swift
+++ VGLabel/ViewController.swift
@@ -13,12 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let text = "<font color='#7f7f7f' size=12>测试测试测试</font><font color='#2092db' size=12><a href='https://www.google.com/'>Goole</a></font><font color='#7f7f7f' size=12>，测试一下VGLabel，参考自RTLabel。</font>"
-        let label = VGLabel()
+        let label = VGLabel(frame: CGRect(origin: CGPoint(x: 5, y: 100), size: CGSize(width: view.frame.width, height: 18)))
         label.text = text
         label.delegate = self
         label.paragraphReplacement = ""
         print(label.optimumSize ?? "xxxx")
-        label.frame = CGRect(origin: CGPoint(x: 0, y: 100), size: label.optimumSize!)
+//        label.frame = CGRect(origin: CGPoint(x: 0, y: 100), size: label.optimumSize!)
         view.addSubview(label)
     }
 }
